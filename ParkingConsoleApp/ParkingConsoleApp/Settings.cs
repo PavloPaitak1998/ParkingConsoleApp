2625557cad69c921b89b3016189a15247c2eaa78
PavloPaitak <38991419+PavloPaitak@users.noreply.github.com>
2018-05-09 18:22:53 +0300

    Added members class Settings

--- ParkingConsoleApp/ParkingConsoleApp/Settings.cs
+++ ParkingConsoleApp/ParkingConsoleApp/Settings.cs
@@ -6,7 +6,26 @@ using System.Threading.Tasks;
 
 namespace ParkingConsoleApp
 {
-    class Settings
+    public static class Settings
     {
+        public static int TimeOut { get; set; }
+        public static Dictionary<CarType, int> Price { get; set; }
+        public static int ParkingSpace { get; set; }
+        public static double Fine { get; set; }
+
+        static Settings()
+        {
+            Price = new Dictionary<CarType, int>
+            {
+                { CarType.Bus, 2 },
+                { CarType.Motorcycle, 1 },
+                { CarType.Passenger, 3 },
+                { CarType.Truck, 5 }
+            };
+
+            TimeOut = 3000;
+            ParkingSpace = 3;
+            Fine = 2;
+        }
     }
 }
