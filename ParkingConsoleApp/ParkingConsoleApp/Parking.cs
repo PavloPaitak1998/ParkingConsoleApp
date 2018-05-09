4c2e34f6049108880676c3ccbe5f23a85f4c19b2
PavloPaitak <38991419+PavloPaitak@users.noreply.github.com>
2018-05-09 18:29:51 +0300

    Realized pattern Singelton in class Parking

--- ParkingConsoleApp/ParkingConsoleApp/Parking.cs
+++ ParkingConsoleApp/ParkingConsoleApp/Parking.cs
@@ -8,6 +8,13 @@ namespace ParkingConsoleApp
 {
     class Parking
     {
+        readonly object locker = new object();
+        private static readonly Lazy<Parking> lazy = new Lazy<Parking>(() => new Parking());
+        public static Parking Instance { get { return lazy.Value; } }
+        private Parking()
+        {
+        }
+
         public List<ICar> Cars { get; private set; }
         public List<Transaction> Transactions { get; private set; }
         public double Balance { get; private set; }
