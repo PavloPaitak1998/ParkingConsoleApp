14a8d355d6ecaed2333324f4f773d24a56ea95c3
PavloPaitak <38991419+PavloPaitak@users.noreply.github.com>
2018-05-10 00:25:45 +0300

    Created new class OutOfBalanceException

---
+++ ParkingConsoleApp/ParkingConsoleApp/OutOfBalanceException.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ParkingConsoleApp
+{
+    class OutOfBalanceException : Exception
+    {
+        public OutOfBalanceException(string message)
+       : base(message)
+        { }
+
+    }
+}
